a59cc39916584ed97147e80db45a4daca8bd6392
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2019-01-28 07:40:52 -0600

    Setup Interceptors

--- src/main.js
+++ src/main.js
@@ -11,8 +11,23 @@ import './registerServiceWorker'
 // Setup Router
 import VueRouter from 'vue-router'
 
+// Setup axios globaly
 axios.defaults.baseURL = 'https://workflow-7c092.firebaseio.com'
 axios.defaults.headers.common['Authorization'] = 'fasdaf'
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+// Setup Axios interceptors
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptors', config)
+  return config
+})
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response Interceptors', res)
+  return res
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
 // Vue.prototype.$http  =  axios
 // const  accessToken  =  localStorage.getItem('access_token')
 //

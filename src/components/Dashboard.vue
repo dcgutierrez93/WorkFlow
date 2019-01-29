48594ec9e42992194131911022efb7aa952ca155
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2019-01-29 12:03:28 -0600

    updated navigation drawer

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,39 +1,69 @@
+<style lang="css" scoped>
+
+.display-header {
+  background-color: #f2f2f2;
+}
+
+</style>
+
 <template lang="html">
-  <div id="dashboard">
-    <h1>Dashboard</h1>
-    <div class="" v-for="(value, key) in folder">
-      {{ key }}: {{ value }}
+
+<div id="dashboard">
+    <div class="container">
+        <h1>Dashboard</h1>
+        <div class="card">
+          <div class="cardbody">
+            <table class="table">
+                <thead>
+                    <tr class="display-header">
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                    </tr>
+                </tbody>
+            </table>
+          </div>
+        </div>
     </div>
-  </div>
+</div>
+
 </template>
 
 <script>
+
 import axios from 'axios'
 
 export default {
-  data() {
-    return {
-      folder: {
-        folderName: "Project 1",
-        files: [
-          "file one",
-          "file two",
-          "file three"
-        ]
-      }
-    }
-  },
-  computed: {
-    email() {
-      return !this.$store.getters.user ? false : this.$store.getters.user.email
-    }
-  },
-  created() {
-    this.$store.dispatch('fetchUser')
-  }
+    data() {
+            return {
+
+            }
+        },
+        computed: {
+            email() {
+                return !this.$store.getters.user ? false : this.$store.getters.user.email
+            }
+        },
+        created() {
+            this.$store.dispatch('fetchUser')
+        }
 }
+
 </script>
-
-<style lang="css" scoped>
-
-</style>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,59 +1,94 @@
+<style lang="css" scoped>
+
+
+
+</style>
 
 <template>
-  <v-navigation-drawer
-     :mini-variant.sync="mini"
-     v-model="drawer"
-     hide-overlay
-     stateless
-     app
-   >
-     <v-toolbar flat class="transparent">
-       <v-list class="pa-0">
-         <v-list-tile avatar>
-           <v-list-tile-avatar>
-             <v-icon>home</v-icon>
-           </v-list-tile-avatar>
 
-           <v-list-tile-content>
-             <v-list-tile-title>WorkFlow</v-list-tile-title>
-           </v-list-tile-content>
+<v-navigation-drawer :mini-variant.sync="mini" v-model="drawer" hide-overlay stateless app>
+    <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+            <v-list-tile avatar>
+                <v-list-tile-avatar>
+                    <v-icon>home</v-icon>
+                </v-list-tile-avatar>
 
-           <v-list-tile-action>
-             <v-btn
-               icon
-               @click.stop="mini = !mini"
-             >
-               <v-icon>chevron_left</v-icon>
-             </v-btn>
-           </v-list-tile-action>
-         </v-list-tile>
-       </v-list>
-     </v-toolbar>
+                <v-list-tile-content>
+                    <v-list-tile-title>WorkFlow</v-list-tile-title>
+                </v-list-tile-content>
 
-     <v-list class="pt-0" dense>
-       <v-divider></v-divider>
+                <v-list-tile-action>
+                    <v-btn icon @click.stop="mini = !mini">
+                        <v-icon>chevron_left</v-icon>
+                    </v-btn>
+                </v-list-tile-action>
+            </v-list-tile>
+        </v-list>
+    </v-toolbar>
 
-       <v-list-tile
-         v-for="item in items"
-         :key="item.title"
-         :to="item.path"
+    <v-list class="pt-0" dense>
+        <v-divider></v-divider>
 
-       >
-         <v-list-tile-action>
-           <v-icon>{{ item.icon }}</v-icon>
-         </v-list-tile-action>
+        <v-list-tile v-for="item in items" :key="item.title" :to="item.path">
+            <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
 
-         <v-list-tile-content>
-           <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-         </v-list-tile-content>
-       </v-list-tile>
+            <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+        </v-list-tile>
 
-       <v-divider></v-divider>
+        <v-divider></v-divider>
 
-       <v-list-tile
+        <!-- New Folder -->
+        <v-list-tile>
+
+          <v-list-tile-action>
+            <v-icon>folder</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>
+              My Projects
+            </v-list-tile-title>
+          </v-list-tile-content>
+
+        </v-list-tile>
+
+        <v-list-tile>
+
+          <v-list-tile-action>
+            <v-icon>create_new_folder</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>
+              New Folder
+            </v-list-tile-title>
+          </v-list-tile-content>
+
+        </v-list-tile>
+
+        <v-list-tile>
+
+          <v-list-tile-action>
+            <v-icon>cloud_upload</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>
+              New File
+            </v-list-tile-title>
+          </v-list-tile-content>
+
+        </v-list-tile>
+
+    <!-- List User Actions - NEW FILE, NEW FOLDER ETC. -->
+    <!-- <v-list-tile
          v-for="userAction in userActions"
          :key="userAction.title"
-
 
        >
          <v-list-tile-action>
@@ -63,59 +98,63 @@
          <v-list-tile-content>
            <v-list-tile-title>{{ userAction.title }}</v-list-tile-title>
          </v-list-tile-content>
-       </v-list-tile>
+       </v-list-tile> -->
+    <!-- List User Actions - NEW FILE, NEW FOLDER ETC. -->
 
-     </v-list>
+    </v-list>
 
-   </v-navigation-drawer>
+</v-navigation-drawer>
 
 </template>
 
-<style lang="css" scoped>
-
-</style>
-
 <script>
+
 import axios from 'axios'
 
 export default {
-  name: 'Header',
-  data () {
-    return {
-      drawer: true,
-      items: [
-        { title: 'Home', icon: 'dashboard', path: '/' },
-        { title: 'Dashboard', icon: 'account_circle', path: '/dashboard'  }
-        // { title: 'New Folder', icon: 'create_new_folder' },
-        // { title: 'New File', icon: 'cloud_upload' }
-      ],
-      userActions: [
-        { title: 'My Projects', icon: 'folder' },
-        { title: 'New Folder', icon: 'create_new_folder' },
-        { title: 'New File', icon: 'cloud_upload' }
-      ],
-      // cruds: [
-      //   ['New Folder', 'add'],
-      //   ['New File', 'insert_drive_file'] //,
-      //   // ['Update', 'update'],
-      //   // ['Delete', 'delete']
-      // ],
-      mini: true,
-      right: null
-    }
-  },
-  computed: {
-    auth() {
-      return this.$store.getters.isAuthenticated
-    }
-  },
-  created() {
-    this.$store.dispatch('fetchUser')
-  }//,
-  // computed: {
-  //   auth() {
-  //     return this.$store.getters.isAuthenticated
-  //   }
-  // }
+    name: 'Header',
+    data() {
+        return {
+            drawer: true,
+            items: [{
+                    title: 'Home',
+                    icon: 'dashboard',
+                    path: '/'
+                }, {
+                    title: 'Dashboard',
+                    icon: 'account_circle',
+                    path: '/dashboard'
+                }
+                // { title: 'New Folder', icon: 'create_new_folder' },
+                // { title: 'New File', icon: 'cloud_upload' }
+            ],
+            // userActions: [{
+            //     title: 'My Projects',
+            //     icon: 'folder'
+            // }, {
+            //     title: 'New Folder',
+            //     icon: 'create_new_folder'
+            // }, {
+            //     title: 'New File',
+            //     icon: 'cloud_upload'
+            // }],
+            mini: true,
+            right: null
+        }
+    },
+    computed: {
+        auth() {
+            return this.$store.getters.isAuthenticated
+        }
+    },
+    created() {
+        this.$store.dispatch('fetchUser')
+    } //,
+    // computed: {
+    //   auth() {
+    //     return this.$store.getters.isAuthenticated
+    //   }
+    // }
 }
+
 </script>

4d4371e1b648b6b54de7980fae0c2b8c14626c38
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2019-01-29 09:20:03 -0600

    Updated Header display for users logged in

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -36,8 +36,8 @@
        <v-list-tile
          v-for="item in items"
          :key="item.title"
-         :to="item.link"
-         @click=""
+         :to="item.path"
+
        >
          <v-list-tile-action>
            <v-icon>{{ item.icon }}</v-icon>
@@ -47,96 +47,60 @@
            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
          </v-list-tile-content>
        </v-list-tile>
+       <v-list-group sub-group no-action v-if="auth">
+           <v-list-tile slot="activator">
+               <v-list-tile-title>Actions</v-list-tile-title>
+           </v-list-tile>
+
+           <v-list-tile v-for="(crud, i) in cruds" :key="i" @click="">
+               <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
+               <v-list-tile-action>
+                   <v-icon v-text="crud[1]"></v-icon>
+               </v-list-tile-action>
+           </v-list-tile>
+       </v-list-group>
      </v-list>
+
    </v-navigation-drawer>
-    <!-- <v-navigation-drawer
-      :mini-variant.sync="mini"
-      v-model="drawer"
-      hide-overlay
-      stateless
-      app
-    >
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <v-icon>home</v-icon>
-            </v-list-tile-avatar>
-
-            <v-list-tile-content>
-              <v-list-tile-title>WorkFlow</v-list-tile-title>
-            </v-list-tile-content>
-
-            <v-list-tile-action>
-              <v-btn
-                icon
-                @click.stop="mini = !mini"
-              >
-                <v-icon>chevron_left</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-      <v-list dense>
-        <v-list-tile @click="" to="/">
-          <v-list-tile-action>
-            <v-icon>home</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="" to="/login">
-          <v-list-tile-action to="register">
-            <v-icon>contact_mail</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Contact</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-
-        <v-list-tile
-          v-for="item in items"
-          :key="item.title"
-          :to="item.link"
-          @click=""
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list> -->
-    <!-- </v-navigation-drawer> -->
 
 </template>
 
 <style lang="css" scoped>
 
 </style>
+
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Header',
   data () {
     return {
       drawer: true,
       items: [
-        { title: 'Home', icon: 'dashboard', link: '/' },
-        { title: 'Dashboard', icon: 'account_circle', link: '/dashboard'  },
-        { title: 'New Folder', icon: 'create_new_folder' },
-        { title: 'New File', icon: 'cloud_upload' }
+        { title: 'Home', icon: 'dashboard', path: '/' },
+        { title: 'Dashboard', icon: 'account_circle', path: '/dashboard'  }
+        // { title: 'New Folder', icon: 'create_new_folder' },
+        // { title: 'New File', icon: 'cloud_upload' }
+      ],
+      cruds: [
+        ['Create', 'add'],
+        ['Read', 'insert_drive_file'],
+        ['Update', 'update'],
+        ['Delete', 'delete']
       ],
       mini: true,
       right: null
     }
-  } //,
+  },
+  computed: {
+    auth() {
+      return this.$store.getters.isAuthenticated
+    }
+  },
+  created() {
+    this.$store.dispatch('fetchUser')
+  }//,
   // computed: {
   //   auth() {
   //     return this.$store.getters.isAuthenticated
